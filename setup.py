2fca0898f2dbbe87b74de57d279c26cda300293e
Navid Matin Moghaddam <navid.matinmo@gmail.com>
2024-05-27 13:16:20 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='utilityai',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'onnxruntime-genai==0.2.0rc7',
+        'numpy',
+    ],
+    author='Navid Matin Moghaddam',
+    author_email='navid.matinmo@gmail.com',
+    description='UtilityAI package for python',
+    url='https://github.com/your_username/your_package',
+)
