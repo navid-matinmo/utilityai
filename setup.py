f808d1b3ee6e6dfc056b53329e52062fff444c75
Navid Matin Moghaddam <navid.matinmo@gmail.com>
2024-05-28 18:08:19 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 from setuptools import setup, find_packages
 
+setup(
+    name='utilityai',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'onnxruntime-genai==0.2.0rc7',
+        'numpy',
+        'huggingface_hub'
+    ],
+    author='Navid Matin Moghaddam',
+    author_email='navid.matinmo@gmail.com',
+    description='UtilityAI package for python',
+    url='https://github.com/navid-matinmo/utilityai',
+)
+
 def download_model():
     from huggingface_hub import hf_hub_download
     import json
@@ -18,20 +33,5 @@ def download_model():
     filenames = info["filenames"]
     for filename in filenames:
         hf_hub_download(repo_id=repo_id, filename=filename, local_dir=local_dir, use_auth_token="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")
-
-setup(
-    name='utilityai',
-    version='1.0.0',
-    packages=find_packages(),
-    install_requires=[
-        'onnxruntime-genai==0.2.0rc7',
-        'numpy',
-        'huggingface_hub'
-    ],
-    author='Navid Matin Moghaddam',
-    author_email='navid.matinmo@gmail.com',
-    description='UtilityAI package for python',
-    url='https://github.com/navid-matinmo/utilityai',
-)
-
+        
 download_model()
