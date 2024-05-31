a0956d81735a2ab288aa5d36c0a30d665970cc19
Navid Matin Moghaddam <navid.matinmo@gmail.com>
2024-05-31 07:38:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ setup(
     name='utilityai',
     version='1.0.0',
     packages=find_packages(),
-    include_package_data=True,
-    package_data={
-        'utilityai': ['model/*'],
-    },
     install_requires=[
         'onnxruntime-genai==0.2.0rc7',
         'numpy',
@@ -21,18 +17,25 @@ setup(
     url='https://github.com/navid-matinmo/utilityai',
 )
 
+def get_model_folder():
+    import os
+    module_dir = os.path.dirname(__file__)
+    model_folder = os.path.join(module_dir, 'data')
+    if not os.path.exists(model_folder):
+        os.makedirs(model_folder)
+    return model_folder
+
 def download_model():
     from huggingface_hub import hf_hub_download
-    import os
-    print(os.getcwd())
     import json
-    local_dir = "./model"
+    import os
+    local_dir = get_model_folder()
 
     info_repo_id = "navid-matinmo/utilityai"
     info_filename = "info.json"
     hf_hub_download(repo_id=info_repo_id, filename=info_filename, local_dir=local_dir, use_auth_token="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")
     
-    file_path = './model/info.json'
+    file_path = os.path.join(local_dir, "info.json")
     with open(file_path, 'r') as file:
         info = json.load(file)
 
@@ -40,4 +43,10 @@ def download_model():
     filenames = info["filenames"]
     for filename in filenames:
         hf_hub_download(repo_id=repo_id, filename=filename, local_dir=local_dir, use_auth_token="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")
+
 download_model()
+
+    # include_package_data=True,
+    # package_data={
+    #     'utilityai': ['model/*'],
+    # },
