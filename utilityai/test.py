8953901fecace8603f4efd5a528e40dcd5e821bc
Navid Matin Moghaddam <navid.matinmo@gmail.com>
2024-06-01 10:55:14 -0700

    update

--- utilityai/test.py
+++ utilityai/test.py
@@ -1,105 +1,116 @@
-# from utilityai.chat import message
-# from utilityai.code import InputData, function
+from utilityai.chat import message
+from utilityai.code import InputData, function
+from utilityai.model import download
 
-# # download the model once after installation
-# from utilityai.model import download
-# download()
+print("Download the model once after installation.")
+download()
+print("************************************************************")
 
-# # message and ask anything
-# message("how to transpose a pytorch tensor?")
+print("Message and ask anything.")
+message("how to transpose a pytorch tensor?")
+print("************************************************************")
 
-# # chat and have a conversation
-# r1, c1 = message("why does mutable and immutable mean")
-# print()
-# print("-------------- next message --------------")
-# print()
-# message("give some examples", c1)
+print("Chat and have a conversation.")
+r1, c1 = message("why does mutable and immutable mean")
+print()
+print("-------------- next message --------------")
+print()
+message("give some examples", c1)
+print("************************************************************")
 
-# # chat about a function
-# def list_sum(numbers):
-#     return sum(numbers)
-# r1, c1 = message("what does this do?", attachment=list_sum)
-# print()
-# print("-------------- next message --------------")
-# print()
-# message("return min and max of numbers too", c1)
+print("Chat about a function.")
+def list_sum(numbers):
+    return sum(numbers)
+r1, c1 = message("what does this do?", attachment=list_sum)
+print()
+print("-------------- next message --------------")
+print()
+message("return min and max of numbers too", c1)
+print("************************************************************")
 
-# # chat about a numpy array
-# import numpy as np
-# array = np.array([[1, 2, 3, 4], 
-#                   [5, 6, 7, 8], 
-#                   [9, 10, 11, 12]])
-# r1, c1 = message("each row is salaries of a person. how to get average salary of each person in an array", attachment=array)
-# print()
-# print("-------------- next message --------------")
-# print()
-# message("what about age?", c1)
+print("Chat about a numpy array.")
+import numpy as np
+array = np.array([[1, 2, 3, 4], 
+                  [5, 6, 7, 8], 
+                  [9, 10, 11, 12]])
+r1, c1 = message("each row is salaries of a person. how to get average salary of each person in an array", attachment=array)
+print()
+print("-------------- next message --------------")
+print()
+message("what about age?", c1)
+print("************************************************************")
 
-# # chat about a pandas dataframe
-# import pandas as pd
-# data = {
-#     'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
-#     'Age': [24, 27, 22, 32, 29],
-#     'Salary': [50000, 54000, 49000, 62000, 58000],
-#     'Department': ['HR', 'Engineering', 'Marketing', 'Finance', 'Engineering'],
-#     'Joining Date': pd.to_datetime(['2020-01-15', '2019-06-23', '2021-03-01', '2018-11-15', '2020-08-30'])
-# }
-# df = pd.DataFrame(data)
-# r1, c1 = message("write code to get average of salary", attachment=df)
-# print()
-# print("-------------- next message --------------")
-# print()
-# message("how to get average salary of each department?", c1)
+print("Chat about a pandas dataframe.")
+import pandas as pd
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+    'Age': [24, 27, 22, 32, 29],
+    'Salary': [50000, 54000, 49000, 62000, 58000],
+    'Department': ['HR', 'Engineering', 'Marketing', 'Finance', 'Engineering'],
+    'Joining Date': pd.to_datetime(['2020-01-15', '2019-06-23', '2021-03-01', '2018-11-15', '2020-08-30'])
+}
+df = pd.DataFrame(data)
+r1, c1 = message("write code to get average of salary", attachment=df)
+print()
+print("-------------- next message --------------")
+print()
+message("how to get average salary of each department?", c1)
+print("************************************************************")
 
-# # chat about a pytorch tensor
-# import torch
-# tensor = torch.tensor([[1, 2, 3], [4, 5, 6]])
-# r1, c1 = message("how to transpose this tensor", attachment=tensor)
-# print()
-# print("-------------- next message --------------")
-# print()
-# message("how to get the size of the resulting tensor", c1)
+print("Chat about a pytorch tensor.")
+import torch
+tensor = torch.tensor([[1, 2, 3], [4, 5, 6]])
+r1, c1 = message("how to transpose this tensor", attachment=tensor)
+print()
+print("-------------- next message --------------")
+print()
+message("how to get the size of the resulting tensor", c1)
+print("************************************************************")
 
-# # function generation in an interactive way. Calling data() and then give function info.
-# data = InputData()
-# data()
-# function(data)
+# function generation in an interactive way. Calling data() and then give function info.
+print("Generate a function interactively by calling data() first, then provide function information.")
+data = InputData()
+data()
+function(data)
+print("************************************************************")
 
-# # function generation by setting data in the code
-# data = InputData()
-# data_dict = {
-#     'function_name': 'prime_number_checker',
-#     'input_names': ['num'],
-#     'input_types': ['int'],
-#     'output_names': ['is_prime'],
-#     'output_types': ['bool'],
-#     'description': "function that checks if a given number is a prime number",
-#     'test_cases': [
-#         {'inputs': [5], 'outputs': [True]},
-#         {'inputs': [10], 'outputs': [False]},
-#         {'inputs': [17], 'outputs': [True]}
-#     ]
-# }
-# data.set_data(data_dict['function_name'], data_dict['input_names'], data_dict['output_names'], data_dict['input_types'], data_dict['output_types'], data_dict['description'], data_dict['test_cases'])
-# function(data)
+print("Generate a function by setting data within the code.")
+data = InputData()
+data_dict = {
+    'function_name': 'prime_number_checker',
+    'input_names': ['num'],
+    'input_types': ['int'],
+    'output_names': ['is_prime'],
+    'output_types': ['bool'],
+    'description': "function that checks if a given number is a prime number",
+    'test_cases': [
+        {'inputs': [5], 'outputs': [True]},
+        {'inputs': [10], 'outputs': [False]},
+        {'inputs': [17], 'outputs': [True]}
+    ]
+}
+data.set_data(data_dict['function_name'], data_dict['input_names'], data_dict['output_names'], data_dict['input_types'], data_dict['output_types'], data_dict['description'], data_dict['test_cases'])
+function(data)
+print("************************************************************")
 
-# # function generation by giving comment on the result
-# data = InputData()
-# data_dict = {
-#     'function_name': 'vague_function',
-#     'input_names': ['a', 'b'],
-#     'input_types': ['int', 'int'],
-#     'output_names': ['subtract'],
-#     'output_types': ['int'],
-#     'description': "function that subtracts two numbers",
-#     'test_cases': [
-#         {'inputs': [1,2], 'outputs': [1]}
-#     ]
-# }
-# data.set_data(data_dict['function_name'], data_dict['input_names'], data_dict['output_names'], data_dict['input_types'], data_dict['output_types'], data_dict['description'], data_dict['test_cases'])
-# res = function(data, max_tries=1)
-# print()
-# print("-------------- comment --------------")
-# print()
-# res.comment = "actually the smaller number must be subtracted from the larger one"
-# function(data, res)
+print("Generate a function and provide a comment on the result for guided generation.")
+data = InputData()
+data_dict = {
+    'function_name': 'vague_function',
+    'input_names': ['a', 'b'],
+    'input_types': ['int', 'int'],
+    'output_names': ['subtract'],
+    'output_types': ['int'],
+    'description': "function that subtracts two numbers",
+    'test_cases': [
+        {'inputs': [1,2], 'outputs': [1]}
+    ]
+}
+data.set_data(data_dict['function_name'], data_dict['input_names'], data_dict['output_names'], data_dict['input_types'], data_dict['output_types'], data_dict['description'], data_dict['test_cases'])
+res = function(data, max_tries=1)
+print()
+print("-------------- comment --------------")
+print()
+res.comment = "actually the smaller number must be subtracted from the larger one"
+function(data, res)
+print("************************************************************")
